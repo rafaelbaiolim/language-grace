66603a04691e333c72ae0e7aaff89edaf085d594
rafaelbaiolim <rafaelbaiolim@gmail.com>
2018-12-04 21:49:20 -0200

    Atualizado testes com def da main

--- tests/unit/CompillerTest.java
+++ tests/unit/CompillerTest.java
@@ -37,7 +37,7 @@ public class CompillerTest {
             "fib.xml",                  //7
     };
 
-//    private static final String TESTS_XML_RELATIVE_PATH = "tests/unit/cases/" + XML_FILE_NAME[0];
+    private static final String TESTS_XML_RELATIVE_PATH = "tests/unit/cases/" + XML_FILE_NAME[0];
 //    private static final String TESTS_XML_RELATIVE_PATH = "tests/unit/cases/" + XML_FILE_NAME[1];
 //    private static final String TESTS_XML_RELATIVE_PATH = "tests/unit/cases/" + XML_FILE_NAME[2];
 //    private static final String TESTS_XML_RELATIVE_PATH = "tests/unit/cases/" + XML_FILE_NAME[3];
